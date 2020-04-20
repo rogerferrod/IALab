8a1520da4bf0493bb56f4d32e59d71253497726a
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-04-20 23:42:28 +0200

    Fixed wrong path in parser pipeline.

--- src/tools/timetable_maker/pipeline-parser.sh
+++ src/tools/timetable_maker/pipeline-parser.sh
@@ -2,11 +2,11 @@
 # chmod a+x pipeline-parser.sh
 
 # Run Clingo and put output in temporany file, in order to parse the result.
-cd ../../src/asp/
-clingo master.cl > ../../tools/parser/temp.txt
+cd ../../asp/
+clingo master.cl > ../tools/timetable_maker/temp.txt
 
 # Go back to the parser directory.
-cd ../../tools/parser/
+cd ../tools/timetable_maker/
 
 # And run the parser on the temp file, which contains the otput.
 python3 parser.py temp.txt
