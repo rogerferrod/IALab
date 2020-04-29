d315a061b619c5288cb2ebd1a3b148fe7c2ea94c
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-04-29 14:51:20 +0200

    Better pipeline-drive script

--- src/tools/timetable_maker/pipeline-parser-drive.sh
+++ src/tools/timetable_maker/pipeline-parser-drive.sh
@@ -2,22 +2,42 @@
 # chmod a+x pipeline-parser.sh
 
 # Run Clingo and put output in temporany file, in order to parse the result.
+
+# First, move to the master.cl folder
 cd ../../asp/
-clingo master.cl > ../tools/timetable_maker/temp.txt
 
-# Go back to the parser directory.
-cd ../tools/timetable_maker/
-
-# Check your path to your google drive before continue
-echo Check the script path to Google Drive folder.
-while true; do
-    read -p "Do you want to exit, in order to check the right path? " yn
-    case $yn in
-        [Yy]* ) exit;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
+# Select the right path to Google drive, in which the file temp.txt will be 
+# copied after the run of this script.
+echo Please choose your path to Goodle Drive: 
+options=("Lorenzo" "Pio" "Roger" "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Lorenzo")
+            echo "you chose Lorenzo's path to Google Drive"
+            clingo master.cl > /Volumes/GoogleDrive/Il\ mio\ Drive/Magistrale/Corsi/IALAB/Progetto/Progetto\ 19-20/1\ -\ PROLOG\ e\ ASP/test/temp.txt
+            echo "temp.txt successfully copied at the specified location. Exiting from program."
+            break
+            ;;
+        "Pio")
+            echo "you chose Pio's path to Google Drive"
+            echo "This path is currently empty. Exiting the program without copying temp.txt."
+            # TODO: Pio's path to GDrive
+            # clingo master.cl > PATH
+            # echo "temp.txt successfully copied at the specified location. Exiting from program."
+            break
+            ;;
+        "Roger")
+            echo "you chose Roger's path to Google Drive"
+            echo "This path is currently empty. Exiting the program without copying temp.txt."
+            # TODO: Roger's path to GDrive
+            # clingo master.cl > PATH
+            # echo "temp.txt successfully copied at the specified location. Exiting from program."
+            break
+            ;;
+        "Exit")
+            echo "Exiting from program."
+            break
+            ;;
     esac
 done
-
-# Copy the temp.txt file to Lorenzo's Google Drive
-cp -f temp.txt /Volumes/GoogleDrive/Il\ mio\ Drive/Magistrale/Corsi/IALAB/Progetto/Progetto\ 19-20/1\ -\ PROLOG\ e\ ASP/test/
