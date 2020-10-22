c9623f850babd918a035e201d69ad19160f6e200
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-10-22 20:17:27 +0200

    Added JSON parameters for configure the experiment

--- src/uncertainty/src/aimacode/statics/StaticBN.java
+++ src/uncertainty/src/aimacode/statics/StaticBN.java
@@ -15,43 +15,64 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class StaticBN {
+
     public static void main(String[] args) throws IOException {
-        args = new String[5];
-//        args[0] = EliminationAskStatic.TOPOLOGICAL;
-//        args[0] = EliminationAskStatic.MIN_DEGREE;
-        args[0] = EliminationAskStatic.MIN_FILL;
+        args = new String[2];
 
-        args[1] = "false"; // pruning
-        args[2] = "false"; // verbose
-        args[3] = "./input/static/E0_baseline.json"; // queries
+        // Experiments configuration
+        args[0] = "./input/static/E1_ordering.json"; // queries
 
-        // network
-//        args[4] = "earthquake_00";
-//        args[4] = "asia_00";
-//        args[4] = "sachs_00";
-//        args[4] = "alarm_00";
-//        args[4] = "win95pts_00"; // LOOP
-//        args[4] = "insurance_00";
-//        args[4] = "munin_full_00"; // LOOP
-//        args[4] = "pigs_00"; // Exception: Java Heap Space
-//        args[4] = "andes_00"; // LOOP
-        args[4] = "link_00"; // LOOP
+        // Chosen network
+        args[1] = "earthquake_00";
+//        args[1] = "asia_00";
+//        args[1] = "sachs_00";
+//        args[1] = "alarm_00";
+//        args[1] = "win95pts_00"; // LOOP
+//        args[1] = "insurance_00";
+//        args[1] = "munin_full_00"; // LOOP
+//        args[1] = "pigs_00"; // Exception: Java Heap Space
+//        args[1] = "andes_00"; // LOOP
+//        args[1] = "link_00"; // LOOP
 
-        String jsonData = new String(Files.readAllBytes(Paths.get(args[3])));
+        // Reading the experiment's JSON
+        String jsonData = new String(Files.readAllBytes(Paths.get(args[0])));
         JSONObject obj = new JSONObject(jsonData);
-        JSONObject experiment = (JSONObject) obj.get(args[4]);
 
-        String network = experiment.getString("network");
-        String query = experiment.getString("query");
-        String evidences = experiment.getString("evidences");
+        // Reading the experiment configuration
+        JSONObject config = (JSONObject) obj.get("config");
+
+        String configOrder = "";
+        switch (config.getString("order")) {
+            case "TOPOLOGICAL":
+                configOrder = EliminationAskStatic.TOPOLOGICAL;
+                break;
+            case "MIN_DEGREE":
+                configOrder = EliminationAskStatic.MIN_DEGREE;
+                break;
+            case "MIN_FILL":
+                configOrder = EliminationAskStatic.MIN_FILL;
+                break;
+        }
+
+        System.out.println(configOrder);
+
+        boolean configPruning = config.getBoolean("pruning");
+        boolean configVerbose = config.getBoolean("verbose");
+
+        JSONObject queries = (JSONObject) obj.get("queries");
+        JSONObject chosenQuery = (JSONObject) queries.get(args[1]);
+
+        String network = chosenQuery.getString("network");
+        String query = chosenQuery.getString("query");
+        String evidences = chosenQuery.getString("evidences");
 
         List<String> queryInput = Arrays.stream(query.split(",")).collect(Collectors.toList());
         List<String> evidencesInput = Arrays.stream(evidences.split(",")).collect(Collectors.toList());
-        List<String[]> assignements = new ArrayList<>();
-        for (String assignement : evidencesInput) {
-            String[] splits = assignement.split("=");
+        List<String[]> assignments = new ArrayList<>();
+        for (String assignment : evidencesInput) {
+            String[] splits = assignment.split("=");
             if (!splits[0].equals("")) {
-                assignements.add(splits);
+                assignments.add(splits);
             }
         }
 
@@ -63,19 +84,19 @@ public class StaticBN {
 
         // Attention: It only work with the VA's first letter to uppercase!
         RandomVariable[] queryVariables = queryInput.stream().map(vaNames::get).toArray(RandomVariable[]::new);
-        AssignmentProposition[] aps = assignements.stream()
+        AssignmentProposition[] aps = assignments.stream()
                 .map(x -> new AssignmentProposition(vaNames.get(x[0]), x[1])).toArray(AssignmentProposition[]::new);
 
-        if (Boolean.parseBoolean(args[1])){
-//            NetworkPruning pruning = new NetworkPruning(bn, queryVariables, aps);
-//            pruning.updateNetwork(pruning.theorem1(), false, false);
-//            pruning.updateNetwork(pruning.theorem2(), true, false);
-//            pruning.updateNetwork(pruning.pruningEdges(), false, true);
-//
-//            bn = pruning.getNetwork();
+        if (configPruning) {
+            NetworkPruning pruning = new NetworkPruning(bn, queryVariables, aps);
+            pruning.updateNetwork(pruning.theorem1(), false, false);
+            pruning.updateNetwork(pruning.theorem2(), true, false);
+            pruning.updateNetwork(pruning.pruningEdges(), false, true);
+
+            bn = pruning.getNetwork();
         }
 
-        BayesInference inference = new EliminationAskStatic(args[0], Boolean.parseBoolean(args[2]));
+        BayesInference inference = new EliminationAskStatic(configOrder, configVerbose);
 
         long start = System.currentTimeMillis();
         CategoricalDistribution distribution = inference.ask(queryVariables, aps, bn);
