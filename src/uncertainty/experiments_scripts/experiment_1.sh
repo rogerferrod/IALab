355e5f6cdc4a1559b39adbf3ae47cf5b71cc973d
PRFina <pioraffaelefina@outlook.it>
2020-10-23 13:53:06 +0200

    Add script for experiment 1

---
+++ src/uncertainty/experiments_scripts/experiment_1.sh
@@ -0,0 +1,31 @@
+EXPERIMENT="E1"
+OUTFILE=$(pwd)/$EXPERIMENT"_out.txt"
+
+cd ../out/static/
+ORDERING="topological"
+VERBOSE="false"
+JSON="../../input/static/E1_ordering.json"
+NETWORKS="earthquake asia"
+PRUNINGS=("true false false"
+          "false true false"
+          "false false true"
+          "true true true")
+
+
+for network in $NETWORKS
+do 
+    echo "processing..."
+    for i in  0 1 2 3 # loop trough pruning combinantions
+    do
+        echo  "Network:"$network "Order:"$ORDERING "Pruning:"${PRUNINGS[i]}
+        
+        echo __________________________________________________________________>> $OUTFILE
+        echo  "Network:"$network "Order:"$ORDERING "Pruning:"${PRUNINGS[i]} &>> $OUTFILE
+        echo >> $OUTFILE
+        java -jar static.jar $ORDERING $VERBOSE $JSON $network ${PRUNINGS[i]} &>> $OUTFILE
+        
+        echo __________________________________________________________________>> $OUTFILE
+        echo >> $OUTFILE
+        echo >> $OUTFILE
+    done
+done
