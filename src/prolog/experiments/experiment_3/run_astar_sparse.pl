a561edc4a98f9740838010da0281765cd99c6a3c
PRFina <pioraffaelefina@outlook.it>
2020-10-30 19:03:03 +0100

    add prolog experiment 3 scritps

---
+++ src/prolog/experiments/experiment_3/run_astar_sparse.pl
@@ -0,0 +1,11 @@
+:- include('../../mazes/labirintosparse.pl').
+
+:- include('../../azioni.pl').
+:- include('../../utils.pl').
+:- include('../../astar.pl').
+
+:- assert(distance(l2)). 
+:- astar(X), write(X).
+
+:-halt(1).
+
