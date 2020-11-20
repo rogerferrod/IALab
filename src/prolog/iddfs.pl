7cd8f633ff7efb75c69179a107fed77db402ac39
Lorenzo Tabasso <lorenzo.tabasso96@gmail.com>
2020-11-20 12:27:54 +0100

    added comment in idfs

--- src/prolog/iddfs.pl
+++ src/prolog/iddfs.pl
@@ -6,6 +6,9 @@ iterative_deepening_search(Solution):-
 iddfs_aux(Limit, Solution):-
     start(S),
     dfs_aux(S,Limit,[S],Solution),!.
+
+% Se arrivi a eseguirte questa regola significa che la regola a riga 6
+% non s'ha da fare (in questo caso il cut a riga 8 agisce come un if-else).
 iddfs_aux(Limit, Solution):-
     NewLimit is Limit + 1,
     iddfs_aux(NewLimit, Solution).
